2b76c743dcfc2c26e75bed5fef6fcac87c47f101
Kushichka <kushichka@gmail.com>
2024-05-14 19:10:10 +0200

    bug fixed

--- src/components/noResultsFound/index.tsx
+++ src/components/noResultsFound/index.tsx
@@ -1,15 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import { Button } from "../ui/button";
 
 import styles from "./noResultsFound.module.scss";
 
 export const NoResultsFound = () => {
+    const router = useRouter();
+    const goHome = () => {
+        router.replace("/");
+    };
+
     return (
         <section className={styles.wrapper}>
             <h2 className={styles.title}>No results found</h2>
             <Button
-                type="secondary"
-                route="home"
-                replace={true}
+                variant="secondary"
+                clickHandler={goHome}
             >
                 Go Home
             </Button>
